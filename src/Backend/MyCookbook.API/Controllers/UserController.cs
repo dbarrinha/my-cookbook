c0df2e3097a4cdc993787330b086e60501c081c9
dbarrinha <dbarrinha@hotmail.com>
2025-10-04 11:20:27 -0300

    feat: adding register user endpoint

---
+++ src/Backend/MyCookbook.API/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCookbook.Communication.Requests;
+using MyCookbook.Communication.Responses;
+
+namespace MyCookbook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            var response = new ResponseRegisterUserJson
+            {
+                Name = request.Name + " foi criado"
+            };
+
+            return Created(string.Empty, response);
+        }
+    }
+}
---
+++ src/Shared/MyCookbook.Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyCookbook.Communication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
