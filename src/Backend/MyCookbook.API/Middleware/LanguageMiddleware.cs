76aece0006a3d27d7ce2b8377d2ce76c9371cc41
dbarrinha <dbarrinha@hotmail.com>
2025-11-05 19:48:24 -0300

    feat: add Language identification support

---
+++ src/Backend/MyCookbook.API/Middleware/LanguageMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace MyCookbook.API.Middleware
+{
+    public class LanguageMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LanguageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var requestedLanguage = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+            
+            var cultureInfo = new CultureInfo("es");
+            
+            if (!string.IsNullOrWhiteSpace(requestedLanguage))
+            {
+                var firstLanguage = requestedLanguage.Split(',').FirstOrDefault();
+                var languageCode = firstLanguage?.Split(';').FirstOrDefault()?.Trim();
+                
+                if (!string.IsNullOrWhiteSpace(languageCode))
+                {
+                    try
+                    {
+                        cultureInfo = new CultureInfo(languageCode);
+                    }
+                    catch (CultureNotFoundException)
+                    {
+                        cultureInfo = new CultureInfo("en");
+                    }
+                }
+            }
+            
+            CultureInfo.CurrentCulture = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+
+            await _next(context);
+        }
+    }
+}
